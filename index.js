8d5c649a3221765f227093c7a8730e0639bd2f1b
Fahima Bushra <bushra.arifeen@gmail.com>
2024-06-19 04:10:53 -0700

    jwt updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -11,8 +12,29 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.gcmb5gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+function createToken(user){
+  const token = jwt.sign({
+    email: user.email
+  }, 'secret', { expiresIn: '7d' });
+  return token;
+}
 
+function verifyToken(req,res,next){
+const token = req.headers.authorization.split(" ")[1];
+console.log(token);
+const verify = jwt.verify(token, 'secret');
+console.log(verify);
+if(!verify?.email){
+  return res.send('you are not authorized')
+}
+req.user = verify.email;
+  next()
+}
+
+
+
+
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.gcmb5gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 const client = new MongoClient(uri, {
@@ -29,23 +51,27 @@ async function run() {
     const coffeesCollection = client.db("productDB").collection("coffeesCollection");
     const userCollection = client.db("userDB").collection("userCollection");
 
-    app.post('/coffees', async (req,res)=>{
+
+
+    app.post('/coffees',verifyToken, async (req,res)=>{
         const coffeesData = req.body;
         const result = await coffeesCollection.insertOne(coffeesData);
         res.send(result)
     })
-   
+
     app.get('/coffees', async (req,res)=>{
       const coffeesData = coffeesCollection.find();
       const result = await coffeesData.toArray();
       res.send(result);
     })
+
     app.get('/coffees/:id', async (req,res)=>{
       const id = req.params.id;
       const coffeesData =  await coffeesCollection.findOne({_id:new ObjectId(id)});
       res.send(coffeesData);
     })
-    app.patch('/coffees/:id', async (req,res)=>{
+
+    app.patch('/coffees/:id',verifyToken, async (req,res)=>{
       const id = req.params.id;
       const updatedData = req.body;
       const result = await coffeesCollection.updateOne({_id:new ObjectId(id)},
@@ -53,18 +79,57 @@ async function run() {
     );
       res.send(result);
     })
-    app.delete('/coffees/:id', async (req,res)=>{
+
+    app.delete('/coffees/:id',verifyToken,async (req,res)=>{
       const id = req.params.id;
       const result = coffeesCollection.deleteOne({_id:new ObjectId(id)});
       res.send(result);
     })
+
    app.post('/user', async(req,res)=>{
     const user = req.body;
+    const token = createToken(user);
+    console.log(token)
     const isUserExist = await userCollection.findOne({email: user?.email})
     console.log(isUserExist)
-    const result = await userCollection.insertOne(user);
-    res.send(result)
+    if(isUserExist?._id){
+      return res.send({
+        status:"success",
+        message:"login success",
+        token
+      })
+    }
+    await userCollection.insertOne(user);
+    res.send(token)
    })
+
+
+   app.get('/user', async (req,res)=>{
+    const userData = userCollection.find();
+    const result = await userData.toArray();
+    res.send(result);
+  })
+
+
+   app.get('/user/get/:id', async (req,res)=>{
+    const id = req.params.id;
+    const result = await userCollection.findOne({_id:new ObjectId(id)});
+    res.send(result);
+   })
+
+   app.get('/user/:email', async (req,res)=>{
+    const email = req.params.email;
+    const result = await userCollection.findOne({email});
+    res.send(result);
+   })
+
+   app.patch('/user/:email', async (req,res)=>{
+    const email = req.params.email;
+    const userData = req.body;
+    const result = await userCollection.updateOne({email},{$set: userData},{upsert:true});
+    res.send(result);
+   })
+
     
     
     
@@ -81,5 +146,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
